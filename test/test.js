d2e407c6e133c6d046969277f5547a472db01431
jslopeza <jasonlopeza95@gmail.com>
2015-05-13 22:21:18 -0400

    First Test

---
+++ test/test.js
@@ -0,0 +1,19 @@
+var assert = require("assert"); // node.js core module
+var C = require('../cash.js'); 
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so indexOf returns -1
+    })
+  })
+});
+
+describe('Cash Register', function(){
+  describe('Module C', function(){
+    it('should have a getChange Method', function(){
+      assert.equal(typeof C, 'object');
+      assert.equal(typeof C.getChange, 'function');
+    })
+  })
+});  
