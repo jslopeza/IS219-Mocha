1bd259109dcb17b1691972dfe520d299e658e1db
jslopeza <jasonlopeza95@gmail.com>
2015-05-13 22:26:42 -0400

    New test for getChange

--- test/test.js
+++ test/test.js
@@ -18,5 +18,8 @@ describe('Cash Register', function(){
     it('getChange(210,300) should equal [50,20,20]', function(){
     	assert.deepEqual(C.getChange(210,300), [50,20,20]);
 	})
+	it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
+    	assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
+	})
   })
 });  
