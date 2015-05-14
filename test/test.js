fa0d39c9be32c4be19bda093912493846723f2a1
jslopeza <jasonlopeza95@gmail.com>
2015-05-13 22:22:57 -0400

    Updated the tests

--- test/test.js
+++ test/test.js
@@ -15,5 +15,8 @@ describe('Cash Register', function(){
       assert.equal(typeof C, 'object');
       assert.equal(typeof C.getChange, 'function');
     })
+    it('getChange(210,300) should equal [50,20,20]', function(){
+    	assert.deepEqual(C.getChange(210,300), [50,20,20]);
+	})
   })
 });  
